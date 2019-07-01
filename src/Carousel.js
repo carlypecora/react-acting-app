e3e9beb1d312630ee85b2160ac25e2f92c027495
Carly Pecora <carly@carlypecora.com>
2019-07-01 10:41:02 -0400

    working carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -19,24 +19,20 @@ export default class ControlledCarousel extends React.Component {
     };
   }
 
-  // createCarItems = () => {
-  // 	const imgs = [Bridge, Work, Mirror, Bike, Mirror, Boss, Bloom]
-  // 	return imgs.map(img => {
-  // 		return (
-  // 		<Carousel.Item>
-  //         <img
-  //           className="d-block w-100"
-  //           src={img}
-  //           alt="First slide"
-  //         />
-  //         <Carousel.Caption>
-  //           <h3>First slide label</h3>
-  //           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-  //         </Carousel.Caption>
-  //       </Carousel.Item>
-  // 		)
-  // 	})
-  // }
+  createCarItems = () => {
+  	const imgs = [Bridge, Work, Mirror, Bike, Mirror, Boss, Bloom]
+  	return imgs.map(img => {
+  		return (
+  		<Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={img}
+            alt="slide"
+          />
+        </Carousel.Item>
+  		)
+  	})
+  }
 
   handleSelect(selectedIndex, e) {
     this.setState({
@@ -54,45 +50,8 @@ export default class ControlledCarousel extends React.Component {
         direction={direction}
         onSelect={this.handleSelect}
       >
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={Bridge}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {this.createCarItems()}
       </Carousel>
     );
   }
 }
-
